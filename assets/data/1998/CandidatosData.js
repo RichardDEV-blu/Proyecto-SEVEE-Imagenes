6fbf4c24786a805cb3d4c18f019b8b93b025a686
David <jdbsjdbs340@gmail.com>
2026-06-22 23:30:51 -0500

    Agregar Imagen y completar CandidatosData

--- assets/data/1998/CandidatosData.js
+++ assets/data/1998/CandidatosData.js
@@ -25,10 +25,33 @@ const img = (path) => {
 // 1. LEYENDA DE COLORES
 // ============================================================
 
-export const dessertsData = [];
+export const dessertsData = [
+  {
+    porcentaje: "Nivel Bajo",
+    p3: "#F8B3E1",
+  },
+  {
+    porcentaje: "Nivel Medio",
+    p3: "#F06BC0",
+  },
+  {
+    porcentaje: "Nivel Alto",
+    p3: "#E00B97",
+  },
+];
 
 // ============================================================
 // 2. DICCIONARIO DE CANDIDATOS
 // ============================================================
 
-export const candidatoData = [];
+export const candidatoData = [
+  {
+    partido: 3,
+    nombre: "ROSALIA ARTEAGA",
+    url: img("imagenes_presidentes/1998/ROSALIAARTEAGA_1998.png"),
+    logo: img("logos_partidos_politicos/1998/MIRA_1998.png"),
+    color: "#E00B97",
+    nombrePartido: "COMPLETAR", //EL candidato pertenece al partido MIRA
+    json: "COMPLETAR",
+  }
+];
